30481e1c9983b04dce3c86c0f2aa334c73e0e628
Shepb26 <fizzlewizzlegp@gmail.com>
2023-10-10 22:00:15 -0400

    Implemented Jump
    Got jump working, to avoid obstacles

--- IMD4006TermProject/Assets/Scripts/PlayerMovement.cs
+++ IMD4006TermProject/Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 //Functionality for player movement, rotation, orientation, etc
 public class PlayerMovement : MonoBehaviour
 {
+
+    Rigidbody rb;
     //How fast we run. Set to serialized for easy access in the inspector
     [SerializeField] private float speed;
+    [SerializeField] private float jumpAmount;
 
     //Stuff for rotation speed
     public float horizontalSpeed = 2.0f;
@@ -15,7 +18,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -24,6 +27,7 @@ public class PlayerMovement : MonoBehaviour
         Move();
         //Commented this out because it doesn't quite work how I wanted but it's close. Uncomment to see how it works
         //Turn();
+        Jump();
     }
 
     void Move()
@@ -43,5 +47,13 @@ public class PlayerMovement : MonoBehaviour
         float v = verticalSpeed * Input.GetAxis("Mouse Y");
         transform.Rotate(-v, h, 0);
         
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump")){
+            Debug.Log("Jump!");
+            rb.AddForce(Vector3.up * jumpAmount, ForceMode.Impulse);
+        }
     }
 }
