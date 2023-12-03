b4350d8dd1d240cabf9232142d675733a8a2b2e0
Shepb26 <fizzlewizzlegp@gmail.com>
2023-12-03 18:07:43 -0500

    Changed investigation code to reflect new anger decision

--- IMD4006TermProject/Assets/Scripts/Behaviour Trees/Systems/Nodes/TaskChasePlayer.cs
+++ IMD4006TermProject/Assets/Scripts/Behaviour Trees/Systems/Nodes/TaskChasePlayer.cs
@@ -10,34 +10,34 @@ public class TaskChasePlayer : BTNode
     Transform transform;
     NavMeshAgent agent;
     Player player;
-    Enemy thisCharacter;
+    Enemy thisActor;
 
     public TaskChasePlayer(Transform transform, NavMeshAgent enemyMeshAgent, Player player)
     {
         this.transform = transform;
         agent = enemyMeshAgent;
         this.player = player;
-        thisCharacter = agent.GetComponent<Enemy>();
+        thisActor = agent.GetComponent<Enemy>();
     }
 
     protected override NodeState OnRun()
     {
         float waypointDistance = Vector3.Distance(transform.position, player.transform.position);
-        if (agent.speed == thisCharacter.defaultSpeed)
+        if (agent.speed == thisActor.defaultSpeed)
         {
-            if (thisCharacter.angerLevel == Enemy.AngerLevel.INDIFFERENT)
+            if (thisActor.angerLevel == Enemy.AngerLevel.INDIFFERENT)
             {
                 agent.speed = 3.5f;
             }
-            else if (thisCharacter.angerLevel == Enemy.AngerLevel.IRRITATED)
+            else if (thisActor.angerLevel == Enemy.AngerLevel.IRRITATED)
             {
                 agent.speed = 4f;
             }
-            else if (thisCharacter.angerLevel == Enemy.AngerLevel.ANGRY)
+            else if (thisActor.angerLevel == Enemy.AngerLevel.ANGRY)
             {
                 agent.speed = 5f;
             }
-            else if (thisCharacter.angerLevel == Enemy.AngerLevel.FURIOUS)
+            else if (thisActor.angerLevel == Enemy.AngerLevel.FURIOUS)
             {
                 agent.speed = 6f;
             }
@@ -46,18 +46,18 @@ public class TaskChasePlayer : BTNode
         //If we're basically on the player now
         if (waypointDistance < 1)
         {
-            agent.speed = thisCharacter.defaultSpeed;
+            agent.speed = thisActor.defaultSpeed;
             state = NodeState.SUCCESS;
         }
         //If at any point the enemy can no longer see the player, this function fails and we change behaviour
-        else if(!thisCharacter.seesPlayer)
+        else if(!thisActor.seesPlayer)
         {
-            thisCharacter.sawPlayer = true;
-            agent.speed = thisCharacter.defaultSpeed;
+            thisActor.sawPlayer = true;
+            agent.speed = thisActor.defaultSpeed;
             state = NodeState.FAILURE;
         }
         //If we can still see the player but haven't reached them yet
-        else if (waypointDistance >= 1 && thisCharacter.seesPlayer)
+        else if (waypointDistance >= 1 && thisActor.seesPlayer)
         {
             agent.SetDestination(player.transform.position);
             // Debug.Log("going to new position");
